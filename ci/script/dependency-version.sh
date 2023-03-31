dd14affa53bf5c865c6aa7e7c3ebbc2edc7dee7e
gissily <gissily@outlook.com>
2023-03-31 16:07:46 +0800

    plugin update scripts

--- ci/script/dependency-version.sh
+++ ci/script/dependency-version.sh
@@ -8,16 +8,19 @@ set -e
 SCRIPT=`readlink -f "${BASH_SOURCE:-$0}"`
 SCRIPT_DIR_PATH=`dirname ${SCRIPT}`
 CI_DIR_PATH=`dirname ${SCRIPT_DIR_PATH}`
-ROOT_PATH=`dirname ${CI_DIR_PATH}`
+export ROOT_PATH=`dirname ${CI_DIR_PATH}`
 
-file=${ROOT_PATH}/dependencies.properties
 
-KEYS=($(props keys ${file}))
+DEFAULT_FILE=${ROOT_PATH}/dependencies.properties
+
+PROPERTIES_FILE=${1:-${DEFAULT_FILE}}
+
+KEYS=($(props keys ${PROPERTIES_FILE}))
 
 for i in "${!KEYS[@]}"
 do
   key=${KEYS[${i}]}
-  value=$(props value ${file} ${key})
+  value=$(props value ${PROPERTIES_FILE} ${key})
   echo "key: ${key} - value: ${value}"
   ${ROOT_PATH}/mvnw versions:set-property -Dproperty=${key} -DnewVersion=${value} >> /dev/null 2>&1
 done
